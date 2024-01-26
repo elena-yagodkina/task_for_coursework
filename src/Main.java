9fa82b830112535bda89a24dcc1dbb0d914d7e5f
elena_yagodkina <engineerlena@mail.ru>
2024-01-26 23:44:02 +0300

    my_task_fix

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ import java.util.stream.Collectors;
 public class Main {
   public static void main(String[] args) {
     String[] names = {"Anna", "Oleg", "Sergei", "Anna", "Igor", "Alexandra", "Anna"};
-    String uniqueNames = Arrays.stream(names).distinct().collect(Collectors.toList()).toString();
+    String uniqueNames = Arrays.stream(names)
+      .distinct()
+      .collect(Collectors.toList())
+      .toString();
     System.out.println(uniqueNames);
   }
 }
